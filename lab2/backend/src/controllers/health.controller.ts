c9931c0d98095bc0261998def3e3d9238b73631a
Nikita-w1 <ppaptyuq@gmail.com>
2026-06-09 20:53:02 +0300

    Delete lab2 directory

--- lab2/backend/src/controllers/health.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export function healthController(_req: Request, res: Response) {
-  res.status(200).json({ ok: true, service: "lab2-variant5-simple-api" });
-}
--- lab2/backend/src/types/common.ts
+++
@@ -1,9 +0,0 @@
-export interface ValidationDetail {
-  field: string;
-  message: string;
-}
-
-export interface ListResponse<T> {
-  items: T[];
-  total: number;
-}
